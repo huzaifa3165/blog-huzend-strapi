819d503b1026d5653845630de32efa054b596529
huzaifa3165 <83898816+huzaifa3165@users.noreply.github.com>
2023-04-18 14:13:56 +0500

    cloudinary and render configure

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'blog'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'huzend'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: false,
-        },
-        debug: false,
-      },
-    });
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"), // Not required
+      ssl: env.bool("DATABASE_SSL", true),
+    },
+    debug: false,
+  },
+});
+
+// ep-square-poetry-080752.us-east-2.aws.neon.tech
